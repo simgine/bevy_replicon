209056dc09a1a1124165ace4f96f71ac6ddb4c78
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-10-05 19:11:20 +0300

    Rename `E` into `M` for messages

--- src/shared/message/message_fns.rs
+++ src/shared/message/message_fns.rs
@@ -33,7 +33,7 @@ impl UntypedMessageFns {
     /// # Safety
     ///
     /// The caller must ensure that the function is called with the same generics with which this instance was created.
-    pub(super) unsafe fn typed<S, D, E: 'static, I: 'static>(self) -> MessageFns<S, D, E, I> {
+    pub(super) unsafe fn typed<S, D, M: 'static, I: 'static>(self) -> MessageFns<S, D, M, I> {
         // `TypeId` can only be obtained for `'static` types, but we can't impose this requirement because our context has a lifetime.
         // So, we use the `typeid` crate for non-static `TypeId`, as we don't care about the lifetime and only need to check the type.
         // This crate is already used by `erased_serde`, so we don't add an extra dependency.
@@ -53,9 +53,9 @@ impl UntypedMessageFns {
         );
         debug_assert_eq!(
             self.message_id,
-            TypeId::of::<E>(),
+            TypeId::of::<M>(),
             "trying to call message functions with message `{}`, but they were created with `{}`",
-            any::type_name::<E>(),
+            any::type_name::<M>(),
             self.message_name,
         );
         debug_assert_eq!(
@@ -68,10 +68,10 @@ impl UntypedMessageFns {
 
         MessageFns {
             serialize_adapter: unsafe {
-                mem::transmute::<unsafe fn(), AdapterSerializeFn<S, E, I>>(self.serialize_adapter)
+                mem::transmute::<unsafe fn(), AdapterSerializeFn<S, M, I>>(self.serialize_adapter)
             },
             deserialize_adapter: unsafe {
-                mem::transmute::<unsafe fn(), AdapterDeserializeFn<D, E, I>>(
+                mem::transmute::<unsafe fn(), AdapterDeserializeFn<D, M, I>>(
                     self.deserialize_adapter,
                 )
             },
@@ -83,23 +83,23 @@ impl UntypedMessageFns {
     }
 }
 
-impl<S, D, E: 'static, I: 'static> From<MessageFns<S, D, E, I>> for UntypedMessageFns {
-    fn from(value: MessageFns<S, D, E, I>) -> Self {
+impl<S, D, M: 'static, I: 'static> From<MessageFns<S, D, M, I>> for UntypedMessageFns {
+    fn from(value: MessageFns<S, D, M, I>) -> Self {
         // SAFETY: these functions won't be called until the type is restored.
         Self {
             serialize_ctx_id: typeid::of::<S>(),
             serialize_ctx_name: any::type_name::<S>(),
             deserialize_ctx_id: typeid::of::<D>(),
             deserialize_ctx_name: any::type_name::<D>(),
-            message_id: TypeId::of::<E>(),
-            message_name: any::type_name::<E>(),
+            message_id: TypeId::of::<M>(),
+            message_name: any::type_name::<M>(),
             inner_id: TypeId::of::<I>(),
             inner_name: any::type_name::<I>(),
             serialize_adapter: unsafe {
-                mem::transmute::<AdapterSerializeFn<S, E, I>, unsafe fn()>(value.serialize_adapter)
+                mem::transmute::<AdapterSerializeFn<S, M, I>, unsafe fn()>(value.serialize_adapter)
             },
             deserialize_adapter: unsafe {
-                mem::transmute::<AdapterDeserializeFn<D, E, I>, unsafe fn()>(
+                mem::transmute::<AdapterDeserializeFn<D, M, I>, unsafe fn()>(
                     value.deserialize_adapter,
                 )
             },
@@ -118,26 +118,26 @@ impl<S, D, E: 'static, I: 'static> From<MessageFns<S, D, E, I>> for UntypedMessa
 /// Since closures can't be used, we provide adapter functions that accept regular serialization functions.
 /// By default, these adapter functions simply call the passed function, but they can be overridden
 /// to perform the type conversion.
-pub(super) struct MessageFns<S, D, E, I = E> {
-    serialize_adapter: AdapterSerializeFn<S, E, I>,
-    deserialize_adapter: AdapterDeserializeFn<D, E, I>,
+pub(super) struct MessageFns<S, D, M, I = M> {
+    serialize_adapter: AdapterSerializeFn<S, M, I>,
+    deserialize_adapter: AdapterDeserializeFn<D, M, I>,
     serialize: SerializeFn<S, I>,
     deserialize: DeserializeFn<D, I>,
 }
 
-impl<S, D, E> MessageFns<S, D, E, E> {
+impl<S, D, M> MessageFns<S, D, M, M> {
     /// Creates a new instance with default adapter functions.
-    pub(super) fn new(serialize: SerializeFn<S, E>, deserialize: DeserializeFn<D, E>) -> Self {
+    pub(super) fn new(serialize: SerializeFn<S, M>, deserialize: DeserializeFn<D, M>) -> Self {
         Self {
-            serialize_adapter: default_serialize_adapter::<S, E>,
-            deserialize_adapter: default_deserialize_adapter::<D, E>,
+            serialize_adapter: default_serialize_adapter::<S, M>,
+            deserialize_adapter: default_deserialize_adapter::<D, M>,
             serialize,
             deserialize,
         }
     }
 }
 
-impl<S, D, E, I> MessageFns<S, D, E, I> {
+impl<S, D, M, I> MessageFns<S, D, M, I> {
     /// Adds conversion to type `T` before serialization and after deserialiation.
     pub(super) fn with_convert<T: AsRef<I> + From<I>>(self) -> MessageFns<S, D, T, I> {
         MessageFns {
@@ -151,62 +151,62 @@ impl<S, D, E, I> MessageFns<S, D, E, I> {
     pub(super) fn serialize(
         self,
         ctx: &mut S,
-        message: &E,
+        message: &M,
         message_bytes: &mut Vec<u8>,
     ) -> Result<()> {
         (self.serialize_adapter)(ctx, message, message_bytes, self.serialize)
     }
 
-    pub(super) fn deserialize(self, ctx: &mut D, message: &mut Bytes) -> Result<E> {
+    pub(super) fn deserialize(self, ctx: &mut D, message: &mut Bytes) -> Result<M> {
         (self.deserialize_adapter)(ctx, message, self.deserialize)
     }
 }
 
-fn default_serialize_adapter<C, E>(
+fn default_serialize_adapter<C, M>(
     ctx: &mut C,
-    message: &E,
+    message: &M,
     message_bytes: &mut Vec<u8>,
-    serialize: SerializeFn<C, E>,
+    serialize: SerializeFn<C, M>,
 ) -> Result<()> {
     (serialize)(ctx, message, message_bytes)
 }
 
-fn default_deserialize_adapter<C, E>(
+fn default_deserialize_adapter<C, M>(
     ctx: &mut C,
     message: &mut Bytes,
-    deserialize: DeserializeFn<C, E>,
-) -> Result<E> {
+    deserialize: DeserializeFn<C, M>,
+) -> Result<M> {
     (deserialize)(ctx, message)
 }
 
-fn convert_serialize_adapter<C, E: AsRef<I>, I>(
+fn convert_serialize_adapter<C, M: AsRef<I>, I>(
     ctx: &mut C,
-    message: &E,
+    message: &M,
     message_bytes: &mut Vec<u8>,
     serialize: SerializeFn<C, I>,
 ) -> Result<()> {
     (serialize)(ctx, message.as_ref(), message_bytes)
 }
 
-fn convert_deserialize_adapter<C, E: From<I>, I>(
+fn convert_deserialize_adapter<C, M: From<I>, I>(
     ctx: &mut C,
     message: &mut Bytes,
     deserialize: DeserializeFn<C, I>,
-) -> Result<E> {
+) -> Result<M> {
     let message = (deserialize)(ctx, message)?;
-    Ok(E::from(message))
+    Ok(M::from(message))
 }
 
 /// Signature of message serialization functions.
-pub type SerializeFn<C, E> = fn(&mut C, &E, &mut Vec<u8>) -> Result<()>;
+pub type SerializeFn<C, M> = fn(&mut C, &M, &mut Vec<u8>) -> Result<()>;
 
 /// Signature of message deserialization functions.
-pub type DeserializeFn<C, E> = fn(&mut C, &mut Bytes) -> Result<E>;
+pub type DeserializeFn<C, M> = fn(&mut C, &mut Bytes) -> Result<M>;
 
 /// Signature of message adapter serialization functions.
-pub(super) type AdapterSerializeFn<C, E, I> =
-    fn(&mut C, &E, &mut Vec<u8>, SerializeFn<C, I>) -> Result<()>;
+pub(super) type AdapterSerializeFn<C, M, I> =
+    fn(&mut C, &M, &mut Vec<u8>, SerializeFn<C, I>) -> Result<()>;
 
 /// Signature of message adapter deserialization functions.
-pub(super) type AdapterDeserializeFn<C, E, I> =
-    fn(&mut C, &mut Bytes, DeserializeFn<C, I>) -> Result<E>;
+pub(super) type AdapterDeserializeFn<C, M, I> =
+    fn(&mut C, &mut Bytes, DeserializeFn<C, I>) -> Result<M>;
