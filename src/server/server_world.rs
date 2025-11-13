816890205ba04ccfe609adeff3886b38853933f8
Hennadii Chernyshchuk <genaloner@gmail.com>
2025-11-14 01:11:41 +0200

    Move replicated archetypes from the state into a resource
    I plan to re-use it for removals.

--- src/server/server_world.rs
+++ src/server/server_world.rs
@@ -1,8 +1,6 @@
-use core::mem;
-
 use bevy::{
     ecs::{
-        archetype::{Archetype, ArchetypeEntity, ArchetypeGeneration, ArchetypeId},
+        archetype::ArchetypeEntity,
         component::{ComponentId, ComponentTicks, StorageType, Tick},
         query::{FilteredAccess, FilteredAccessSet},
         storage::TableId,
@@ -12,12 +10,9 @@ use bevy::{
     prelude::*,
     ptr::Ptr,
 };
-use log::{debug, trace};
+use log::debug;
 
-use crate::{
-    prelude::*,
-    shared::replication::rules::{ReplicationRules, component::ComponentRule},
-};
+use crate::{prelude::*, shared::replication::rules::ReplicationRules};
 
 /// A [`SystemParam`] that wraps [`World`], but provides access only for replicated components.
 ///
@@ -48,6 +43,7 @@ impl<'w> ServerWorld<'w, '_> {
                 .has_component_read(component_id)
         );
 
+        // SAFETY: caller ensured the component is replicated.
         let storages = unsafe { self.world.storages() };
         match storage {
             StorageType::Table => unsafe {
@@ -71,23 +67,6 @@ impl<'w> ServerWorld<'w, '_> {
             },
         }
     }
-
-    /// Return iterator over replicated archetypes.
-    pub(super) fn iter_archetypes(
-        &self,
-    ) -> impl Iterator<Item = (&Archetype, &ReplicatedArchetype)> {
-        self.state.archetypes.iter().map(|replicated_archetype| {
-            // SAFETY: all IDs from replicated archetypes obtained from real archetypes.
-            let archetype = unsafe {
-                self.world
-                    .archetypes()
-                    .get(replicated_archetype.id)
-                    .unwrap_unchecked()
-            };
-
-            (archetype, replicated_archetype)
-        })
-    }
 }
 
 unsafe impl SystemParam for ServerWorld<'_, '_> {
@@ -108,12 +87,7 @@ unsafe impl SystemParam for ServerWorld<'_, '_> {
             }
         }
 
-        Self::State {
-            component_access,
-            marker_id,
-            archetypes: Default::default(),
-            generation: ArchetypeGeneration::initial(),
-        }
+        Self::State { component_access }
     }
 
     fn init_access(
@@ -139,43 +113,6 @@ unsafe impl SystemParam for ServerWorld<'_, '_> {
         world: UnsafeWorldCell<'world>,
         _change_tick: Tick,
     ) -> Self::Item<'world, 'state> {
-        let archetypes = world.archetypes();
-        let old_generation = mem::replace(&mut state.generation, archetypes.generation());
-
-        // SAFETY: Has access to this resource and the access is unique.
-        let rules = unsafe {
-            world
-                .get_resource::<ReplicationRules>()
-                .expect("replication rules should've been initialized in the plugin")
-        };
-        for archetype in archetypes[old_generation..]
-            .iter()
-            .filter(|archetype| archetype.contains(state.marker_id))
-        {
-            trace!("marking `{:?}` as replicated", archetype.id());
-            let mut replicated_archetype = ReplicatedArchetype::new(archetype.id());
-            for rule in rules.iter().filter(|rule| rule.matches(archetype)) {
-                for &component in &rule.components {
-                    // Since rules are sorted by priority,
-                    // we are inserting only new components that aren't present.
-                    if replicated_archetype
-                        .components
-                        .iter()
-                        .any(|(existing, _)| existing.id == component.id)
-                    {
-                        continue;
-                    }
-
-                    // SAFETY: archetype matches the rule, so the component is present.
-                    let storage =
-                        unsafe { archetype.get_storage_type(component.id).unwrap_unchecked() };
-                    replicated_archetype.components.push((component, storage));
-                }
-            }
-
-            state.archetypes.push(replicated_archetype);
-        }
-
         ServerWorld { world, state }
     }
 }
@@ -187,38 +124,10 @@ pub(crate) struct ReplicationReadState {
     ///
     /// Used only in debug to check component access.
     component_access: FilteredAccess,
-
-    /// ID of [`Replicated`] component.
-    marker_id: ComponentId,
-
-    /// Highest processed archetype ID.
-    generation: ArchetypeGeneration,
-
-    /// Archetypes marked as replicated.
-    archetypes: Vec<ReplicatedArchetype>,
-}
-
-/// An archetype that can be stored in [`ReplicatedArchetypes`].
-pub(crate) struct ReplicatedArchetype {
-    /// Associated archetype ID.
-    pub(super) id: ArchetypeId,
-
-    /// Components marked as replicated.
-    pub(super) components: Vec<(ComponentRule, StorageType)>,
-}
-
-impl ReplicatedArchetype {
-    fn new(id: ArchetypeId) -> Self {
-        Self {
-            id,
-            components: Default::default(),
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use bevy::transform::components::Transform;
     use serde::{Deserialize, Serialize};
     use test_log::test;
 
@@ -229,11 +138,11 @@ mod tests {
     #[should_panic]
     fn query_after() {
         let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
+        app.init_resource::<ReplicationRegistry>()
+            .init_resource::<ProtocolHasher>()
             .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<Transform>()
-            .add_systems(Update, |_: ServerWorld, _: Query<&mut Transform>| {});
+            .replicate::<Test>()
+            .add_systems(Update, |_: ServerWorld, _: Query<&mut Test>| {});
 
         app.update();
     }
@@ -242,11 +151,11 @@ mod tests {
     #[should_panic]
     fn query_before() {
         let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
+        app.init_resource::<ReplicationRegistry>()
+            .init_resource::<ProtocolHasher>()
             .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<Transform>()
-            .add_systems(Update, |_: Query<&mut Transform>, _: ServerWorld| {});
+            .replicate::<Test>()
+            .add_systems(Update, |_: Query<&mut Test>, _: ServerWorld| {});
 
         app.update();
     }
@@ -254,172 +163,24 @@ mod tests {
     #[test]
     fn readonly_query() {
         let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<Transform>()
-            .add_systems(Update, |_: ServerWorld, _: Query<&Transform>| {});
-
-        app.update();
-    }
-
-    #[test]
-    fn empty() {
-        let mut app = App::new();
         app.init_resource::<ReplicationRules>()
+            .init_resource::<ProtocolHasher>()
             .init_resource::<ReplicationRegistry>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert!(world.state.archetypes.is_empty());
-            });
+            .replicate::<Test>()
+            .add_systems(Update, |_: ServerWorld, _: Query<&Test>| {});
 
-        app.world_mut().spawn_empty();
-        app.update();
-    }
-
-    #[test]
-    fn no_components() {
-        let mut app = App::new();
-        app.init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert!(archetype.components.is_empty());
-            });
-
-        app.world_mut().spawn(Replicated);
         app.update();
     }
 
     #[test]
     fn not_replicated() {
         let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<A>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert!(archetype.components.is_empty());
-            });
+        app.init_resource::<ReplicationRules>()
+            .add_systems(Update, |_: ServerWorld, _: Query<&mut Test>| {});
 
-        app.world_mut().spawn((Replicated, B));
         app.update();
     }
 
-    #[test]
-    fn component() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<A>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert_eq!(archetype.components.len(), 1);
-            });
-
-        app.world_mut().spawn((Replicated, A));
-        app.update();
-    }
-
-    #[test]
-    fn bundle() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate_bundle::<(A, B)>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert_eq!(archetype.components.len(), 2);
-            });
-
-        app.world_mut().spawn((Replicated, A, B));
-        app.update();
-    }
-
-    #[test]
-    fn part_of_bundle() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate_bundle::<(A, B)>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert!(archetype.components.is_empty());
-            });
-
-        app.world_mut().spawn((Replicated, A));
-        app.update();
-    }
-
-    #[test]
-    fn bundle_with_subset() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<A>()
-            .replicate_bundle::<(A, B)>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert_eq!(archetype.components.len(), 2);
-            });
-
-        app.world_mut().spawn((Replicated, A, B));
-        app.update();
-    }
-
-    #[test]
-    fn bundle_with_multiple_subsets() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate::<A>()
-            .replicate::<B>()
-            .replicate_bundle::<(A, B)>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert_eq!(archetype.components.len(), 2);
-            });
-
-        app.world_mut().spawn((Replicated, A, B));
-        app.update();
-    }
-
-    #[test]
-    fn bundle_with_overlap() {
-        let mut app = App::new();
-        app.init_resource::<ProtocolHasher>()
-            .init_resource::<ReplicationRules>()
-            .init_resource::<ReplicationRegistry>()
-            .replicate_bundle::<(A, C)>()
-            .replicate_bundle::<(A, B)>()
-            .add_systems(Update, |world: ServerWorld| {
-                assert_eq!(world.state.archetypes.len(), 1);
-                let archetype = world.state.archetypes.first().unwrap();
-                assert_eq!(archetype.components.len(), 3);
-            });
-
-        app.world_mut().spawn((Replicated, A, B, C));
-        app.update();
-    }
-
-    #[derive(Serialize, Deserialize, Component)]
-    struct A;
-
-    #[derive(Serialize, Deserialize, Component)]
-    struct B;
-
-    #[derive(Serialize, Deserialize, Component)]
-    struct C;
+    #[derive(Component, Serialize, Deserialize)]
+    struct Test;
 }
