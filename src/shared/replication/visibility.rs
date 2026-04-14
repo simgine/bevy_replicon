f8d04a5d113354ca41671ce53832f630d1b72504
malfuu <256192511+malfuu@users.noreply.github.com>
2026-04-14 02:16:16 +0200

    remove conditional doc

--- src/shared/replication/visibility.rs
+++ src/shared/replication/visibility.rs
@@ -8,10 +8,7 @@ use crate::shared::replication::registry::{
 
 /// Component that controls remote entity visibility.
 ///
-#[cfg_attr(
-    doc,
-    doc = "Should be registered via [`crate::server::visibility::AppVisibilityExt`]."
-)]
+/// Should be registered via [`crate::server::visibility::AppVisibilityExt`].
 pub trait VisibilityFilter: Component<Mutability = Immutable> {
     /**
     Component on the client entity that will be passed to [`Self::is_visible`].
