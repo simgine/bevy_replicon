c6cbf5e9efc7d96cd7f88b506e53f5c7194ab386
Morb0 <14136326+Morb0@users.noreply.github.com>
2026-05-13 20:54:56 +0300

    fix test

--- tests/broadcast_message.rs
+++ tests/broadcast_message.rs
@@ -143,7 +143,7 @@ fn without_plugins() {
         .drain()
         .collect();
     assert_eq!(broadcasts.len(), 1);
-    assert!(broadcasts[0].broadcaster.is_remote());
+    assert!(matches!(broadcasts[0].broadcaster, Broadcaster::Remote(_)));
 }
 
 #[test]
